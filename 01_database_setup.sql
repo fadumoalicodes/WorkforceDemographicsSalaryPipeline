99c178ffb4bffddd1956de97f0ead81856eb913e
fadumoalicodes <FATIMA.OMAR1234@OUTLOOK.COM>
2026-07-29 23:55:25 +0100

    Create tables for employee demographics and salary

---
+++ 01_database_setup.sql
@@ -0,0 +1,19 @@
+CREATE TABLE EMPLOYEEDEMOGRAPHICS (
+    EmployeeID INT, 
+    FirstName VARCHAR(50), 
+    Age INT, 
+    Gender VARCHAR(10)
+);
+
+CREATE TABLE WarehouseEmployeeDemographics (
+    EmployeeID INT, 
+    FirstName VARCHAR(50), 
+    Age INT, 
+    Gender VARCHAR(10)
+);
+
+CREATE TABLE EmployeeSalary (
+    EmployeeID INT, 
+    JobTitle VARCHAR(50), 
+    Salary INT
+);
